3139cca83b27c844d524d18c2efe19be09dd64f0
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2024-03-01 15:26:53 -0800

    Add test image

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './style.css';
+import testImage from './images/man.jpg';
+
+// 1 image can be imported. Let's see how to import everything
+const docBody = document.querySelector('body');
+
+const man = document.createElement('img');
+man.src = testImage;
+
+docBody.appendChild(man);
