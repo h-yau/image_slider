6daa3c72c3686b9054f8b8f9db17127c9c73bae7
Hin Yau <99358826+nzxzn@users.noreply.github.com>
2024-03-28 17:57:00 -0700

    Modify buttons and stack images

--- src/index.js
+++ src/index.js
@@ -13,33 +13,43 @@ const populateImages = () => {
   );
 
   srcImages.forEach((srcImage) => {
+    const slide = document.createElement('li');
+    slide.classList.add('slide');
+
     const image = document.createElement('img');
     image.src = srcImage;
     image.classList.add('slider-image');
-    image.style.height = '20rem';
 
-    imagesToAppend.push(image);
+    slide.appendChild(image);
+
+    imagesToAppend.push(slide);
   });
   return imagesToAppend;
 };
 
 const createArrow = (arrow) => {
+  const arrowDiv = document.createElement('div');
+  arrowDiv.classList.add('arrow-div', arrow);
+
   const arrowButton = document.createElement('button');
   arrowButton.type = 'button';
-  arrowButton.textContent = arrow;
-  arrowButton.classList.add('arrow', arrow);
+  arrowButton.classList.add('arrow');
 
-  return arrowButton;
+  arrowDiv.appendChild(arrowButton);
+
+  return arrowDiv;
 };
 
 const createImageSlider = () => {
-  const imageSlider = document.createElement('div');
+  const imageSlider = document.createElement('ul');
   imageSlider.classList.add('image-slider');
 
   const imagesToAppend = populateImages();
   imagesToAppend.forEach((image) => {
     imageSlider.appendChild(image);
   });
+
+  imageSlider.querySelector('.slider-image').classList.add('current-image');
 
   imageSlider.appendChild(createArrow('left'));
   imageSlider.appendChild(createArrow('right'));
